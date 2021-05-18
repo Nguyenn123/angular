f5042f72988a46a6654c55fc9dc5aa9690074639
Nguyen Tran <dongcoxanh1001@gmail.com>
2021-05-18 22:02:24 +0700

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  photo : any
+  private apiURL = 'https://jsonplaceholder.typicode.com/todos/1'
+  constructor(private http: HttpClient) {
+
+  }
+
+  getData() : Observable<any>{
+    return this.http.get(this.apiURL)
+  }
+
+  getPhoto() {
+    this.getData().subscribe((res: any)=> {
+      console.log(res);
+      this.photo = res
+    })
+  }
 }
+
